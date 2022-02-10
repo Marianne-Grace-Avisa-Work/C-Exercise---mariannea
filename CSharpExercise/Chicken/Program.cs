6bfeaaf20989a43670e20e17428ea8e408922ebd
Marianne-Grace-Avisa-Work <99171441+Marianne-Grace-Avisa-Work@users.noreply.github.com>
2022-02-10 18:43:34 +0800

    Add files via upload

---
+++ CSharpExercise/Chicken/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Chicken
+{
+    public interface IBird
+    {
+        Egg Lay();
+    }
+
+    // Chicken implements the IBird interface
+    public class Chicken : IBird
+    {
+        public Chicken()
+        {
+            Console.WriteLine("I'm a chicken");
+        }
+
+        public Egg Lay(){
+            //A Chicken lays an egg that will hatch into a new Chicken.
+            return new Egg(new Func<Chicken>(() => new Chicken()));
+        }
+    }
+
+    public class Egg
+    {
+        bool hatch = false;
+        Func<IBird> create;
+
+        public Egg(Func<IBird> createBird)
+        {
+            this.create = createBird;
+        }
+
+        public IBird Hatch()
+        {
+            if (!hatch)
+            {
+                Console.WriteLine("I have hatched an egg");
+                hatch = true;
+                return this.create();
+            }
+            else
+                throw new System.InvalidOperationException();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var chicken1 = new Chicken();
+            var eggChicken = chicken1.Lay();
+            var childChicken = eggChicken.Hatch();
+            eggChicken.Hatch(); //Hatching an egg for the second time throws a System.InvalidOperationException
+
+            Console.ReadLine();
+        }
+    }
+}
